e4eb34e30ae8f9dbc3b10d5cd0563611b4cd13c4
Cameron Smith <Cs11@usf.edu>
2024-10-20 21:36:36 -0400

    Create Book class with properties and methods

---
+++ library_management.js
@@ -0,0 +1,12 @@
+// Task 1: Creating a Book Class
+
+class Book {
+    constructor( title, author, ISBN, _isAvailable)
+     {
+        _title = title;
+        _author = author;
+        _ISBN = ISBN;
+        _isAvailable = _isAvailable;
+        
+    }
+}
